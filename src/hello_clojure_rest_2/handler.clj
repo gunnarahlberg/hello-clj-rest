174c7bf7854b2288920c11c9673677ecff12a134
Gunnar Ahlberg <gunnar.ahlberg@tacton.com>
2015-02-06 00:29:22 +0100

    require setup

--- src/hello_clojure_rest_2/handler.clj
+++ src/hello_clojure_rest_2/handler.clj
@@ -1,7 +1,9 @@
 (ns hello-clojure-rest-2.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.json :refer [wrap-json-response]]
+            [ring.util.response :refer [response]]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
